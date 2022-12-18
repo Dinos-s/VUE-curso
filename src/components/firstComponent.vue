75b5f64494e65e3c4a5eee97a740d23ec6d12a8b
Gabriel <gabriel02ramos@gmail.com>
2022-12-18 18:25:12 -0300

    dados dinamicos

--- src/components/firstComponent.vue
+++ src/components/firstComponent.vue
@@ -1,9 +1,16 @@
 <template>
   <h1>Olá pessoas!</h1>
+  <p>nome: {{ nome }}, trabalho no momento como {{ trabalho }}</p>
 </template>
 
 <script>
 export default {
-  name: 'firstComponent'
+  name: 'firstComponent',
+  data(){
+    return {
+      nome: "Gabriel",
+      trabalho: "Jovem Aprendiz"
+    }
+  }
 }
 </script>
