e397dbe9e88935ed7f4cdc8cf5e8fd502e468d2a
Gabriel <gabriel02ramos@gmail.com>
2022-12-18 17:25:18 -0300

    criando componente

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Iniciando no Vue.JS</h1>
+  <firstComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import firstComponent from "./components/firstComponent.vue";
+  export default {
+    name: 'App',
+    components:{
+      firstComponent
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/firstComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Olá pessoas!</h1>
+</template>
+
+<script>
+export default {
+  name: 'firstComponent'
+}
+</script>
