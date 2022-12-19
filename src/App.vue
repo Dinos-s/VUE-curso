d57a8b6c961a1c56f37ce7ca654ac23ed03964a7
Gabriel <gabriel02ramos@gmail.com>
2022-12-19 10:34:24 -0300

    LifeCycle Vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <h1>Iniciando no Vue.JS</h1>
   <firstComponent/>
+  <LifeCycle/>
 </template>
 
 <script>
 import firstComponent from "./components/firstComponent.vue";
+import LifeCycle from "./components/LifeCycle.vue";
   export default {
     name: 'App',
     components:{
-      firstComponent
-    }
+      firstComponent,
+      LifeCycle,
+    },
   }
 </script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Animal: {{ animal }}</h1>
+</template>
+
+<script>
+    export default {
+        animal: 'Rino',
+        data(){
+            return{
+                animal: 'desconhecido'
+            }
+        },
+        
+        created(){
+            setTimeout(() => {
+                this.animal = "Morcego"
+            }, 1000);
+        },
+
+        mounted(){
+            setTimeout(() => {
+                this.animal = "T-Rex"
+            }, 2000);
+        }
+    }
+</script>
