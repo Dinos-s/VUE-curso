f64584100bcce30ea4640eedf02ff9637d1abf2a
Gabriel <gabriel02ramos@gmail.com>
2023-06-19 10:35:06 -0300

    Compreendendo slots

--- src/components/Aula.vue
+++ src/components/Aula.vue
@@ -1,7 +1,15 @@
 <template>
-  <h2>Aula - 21</h2>
+  <h2>Aula - 22</h2>
   <p>Aula de {{ aula }}</p>
-  <Info/>
+  <Info>
+    <h1>Ententendo e comprenendedo a tag slot</h1>
+    <template v-slot:title>
+      delta 2730
+    </template>
+    <template v-slot:person>
+      <p>Desse modo, podemos trbalhar no compoente pai como se fose um html comum, onde o componete filho terá um dos tipos de slots deixando o seu codigo mais dinamico possivel</p>
+    </template>
+  </Info>
 </template>
 
 <script>
@@ -10,7 +18,7 @@ export default {
   name: 'Aula',
   data(){
     return {
-      aula: "watch/observadores"
+      aula: "Slots"
     }
   },
   components: {
--- src/components/info.vue
+++ src/components/info.vue
@@ -1,60 +1,23 @@
 <template>
-    <!-- <h2>Aula - 21</h2> -->
+    <!-- <h2>Aula - 22</h2> -->
     <div>
-        <p>Sendo declarado na parte <strong>script</strong>, o <strong>watch</strong> é um objeto que obsevará um
-            determinado valor e executará uma determinada função;</p>
-        <p>tendo semelhança com os ganchos de ciclo de vida (Life cycle hooks);</p>
-        <p>No entanto para que funcione, é nescessario term uma função com o mesmo nome da variavel que deseja observar,
-            retornando dois parametros, <strong>NewValue & OldValue</strong></p>
-
-        <p>Os observadores profundos, caso vc mexa com um componente mais interno como <strong>user.lName</strong>, vc deve transformar o watcher um objeto;</p>
-        
-        <p>Os observadores profundos, deixam duas funcioladades visiveis o <strong>handler()</strong>, que é o que vai executar, uma função qualquer. E um <strong>deep(profrundo)</strong> que deve retornar um booleano, que quando qualquer coisa no objeto mudar, exute o handler()</p>
-
-        <input type="text" placeholder="primeiro nome" v-model="user.fName"><br>
-        <input type="text" placeholder="sobrenome" v-model="user.lName">
-        <br>
-        <select v-model="pageCount">
-            <option value="0" disabled>0</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="15">15</option>
-        </select> <br>
-        <img :src="url" alt="pokemon">
-    </div>
+        <slot></slot>
+        <slot name='title'></slot>
+        <p>A tag <strong>slot</strong> pode ser usada para deixar o seu codigo mais fluido e flexivel;</p>
+        <p>Ela pode se declarada em um dos componentes, para isso ela é dividida de duas formas declarativas;</p>
+        <li>Modo padrão: <strong>slot</strong></li>
+        <li>Modo especifico: <strong>slot name=""</strong></li>
+        <h2>
+            <slot name="person"></slot>
+        </h2>
+    </div> 
 </template>
 
 <script>
 export default {
     name: "Info",
-    data() {
-        return {
-            pageCount: '0',
-            user: {
-                fName: '',
-                lName: ''
-            },
-            url: ''
-        }
-    },
-    watch: {
-        pageCount(nv) {
-            if (nv >= 5) {
-                this.savePage()
-            }
-        },
-        user: {
-            handler(){
-                console.log('Tudo ok');
-            },
-            deep: true
-        }
-    },
-    methods: {
-        async savePage() {
-           console.log('merda');
-           this.url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pageCount}.png`
-        }
+    mounted(){
+        console.log(this.$slots);
     }
 }
 </script>
