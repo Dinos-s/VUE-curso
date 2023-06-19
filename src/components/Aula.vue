173406f907cd72b6b17b610b1537209dff2dd5cc
Gabriel <gabriel02ramos@gmail.com>
2023-06-18 21:15:18 -0300

    Watchers / observadores

--- src/components/Aula.vue
+++ src/components/Aula.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>Aula - 20</h2>
+  <h2>Aula - 21</h2>
   <p>Aula de {{ aula }}</p>
   <Info/>
 </template>
@@ -10,7 +10,7 @@ export default {
   name: 'Aula',
   data(){
     return {
-      aula: "Propriedade Computada"
+      aula: "watch/observadores"
     }
   },
   components: {
--- src/components/info.vue
+++ src/components/info.vue
@@ -1,14 +1,26 @@
 <template>
-    <!-- <h2>Aula - 20</h2> -->
+    <!-- <h2>Aula - 21</h2> -->
     <div>
-        <p>De modo resumido, as propriedades computadas são funções que dependem de outros componentes, para o valor final ser apresentado;</p>
-        <p>Durante o uso das props computadas, os dados/valores finais só serão executadas caso um dos valores dependentes seja alterado. já que esses daos são amarzenados em cash.</p>
+        <p>Sendo declarado na parte <strong>script</strong>, o <strong>watch</strong> é um objeto que obsevará um
+            determinado valor e executará uma determinada função;</p>
+        <p>tendo semelhança com os ganchos de ciclo de vida (Life cycle hooks);</p>
+        <p>No entanto para que funcione, é nescessario term uma função com o mesmo nome da variavel que deseja observar,
+            retornando dois parametros, <strong>NewValue & OldValue</strong></p>
 
-        First Name:
-        <input type="text" v-model="user.Fname"><br>
-        Last Name:
-        <input type="text" v-model="user.Lname">
-        <p>Nome completo: {{ fullName }}</p>
+        <p>Os observadores profundos, caso vc mexa com um componente mais interno como <strong>user.lName</strong>, vc deve transformar o watcher um objeto;</p>
+        
+        <p>Os observadores profundos, deixam duas funcioladades visiveis o <strong>handler()</strong>, que é o que vai executar, uma função qualquer. E um <strong>deep(profrundo)</strong> que deve retornar um booleano, que quando qualquer coisa no objeto mudar, exute o handler()</p>
+
+        <input type="text" placeholder="primeiro nome" v-model="user.fName"><br>
+        <input type="text" placeholder="sobrenome" v-model="user.lName">
+        <br>
+        <select v-model="pageCount">
+            <option value="0" disabled>0</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+        </select> <br>
+        <img :src="url" alt="pokemon">
     </div>
 </template>
 
@@ -17,16 +29,31 @@ export default {
     name: "Info",
     data() {
         return {
+            pageCount: '0',
             user: {
-                Fname: '',
-                Lname: ''
-            }
+                fName: '',
+                lName: ''
+            },
+            url: ''
         }
     },
-    computed: {
-        fullName(){
-            return `${this.user.Fname} ${this.user.Lname}`
-            
+    watch: {
+        pageCount(nv) {
+            if (nv >= 5) {
+                this.savePage()
+            }
+        },
+        user: {
+            handler(){
+                console.log('Tudo ok');
+            },
+            deep: true
+        }
+    },
+    methods: {
+        async savePage() {
+           console.log('merda');
+           this.url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pageCount}.png`
         }
     }
 }
